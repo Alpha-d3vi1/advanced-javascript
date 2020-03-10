e919b55cfc411abff3683a3425315dc07cf7034e
Alpha-d3vi1 <devil100052@gmail.com>
2020-03-11 00:16:12 +0600

    double triple equal

---
+++ doublevstriple_equal.js
@@ -0,0 +1,10 @@
+// == is less equal , === more accurate
+const first  = 2;
+const second = "2";
+if(first == second){
+    console.log("Condition True");
+}
+else{
+    console.log("Condition False");
+}
+
