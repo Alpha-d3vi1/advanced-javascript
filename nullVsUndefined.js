800731bfc27a58dcb3c9b7a79268c9f1924c2c20
Alpha-d3vi1 <devil100052@gmail.com>
2020-03-11 00:03:58 +0600

    nullvsundefined cleard

---
+++ nullVsUndefined.js
@@ -0,0 +1,22 @@
+//undefined -->
+
+// let pakhi;
+
+// function add(num1,num2){
+//     console.log(num1,num2);
+//     return
+// }
+
+// function add(num1,num2){
+//     console.log(num1, num2);
+// }
+
+
+// const premik = {name:"Smart dude",phone:6464}
+// console.log(premik.gf);
+
+// let fun = undefined;
+// console.log(fun);
+
+//null -->
+
